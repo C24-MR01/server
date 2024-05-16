71dfa90873a857016324daefe9daca927785058a
Rufus8702 <Jamaludinahmad013@gmail.com>
2024-05-16 20:52:06 +0700

    setup firebase

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
+const {db} = require('../db');
 const express = require('express');
 const app = express();
 
 require('dotenv').config({ path: '.env' });
 
-app.get('/', (req, res) => {
-  res.send("Hello world!");
-})
+app.get('/', async (req, res) => {
+  const querySnapshot = await db.collection('users').get();
+  const users = querySnapshot.docs.map(doc => doc.data());
+  res.json(users);
+});
 
 app.set('port', process.env.PORT || 8888);
 const server = app.listen(app.get('port'), () => {
