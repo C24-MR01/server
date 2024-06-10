054b43f31234b3f1d98587f5e3809a745549a91a
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2024-06-10 17:14:28 +0700

    [FEAT] add get movie by genre

--- src/routes/movieRoute.js
+++ src/routes/movieRoute.js
@@ -3,13 +3,13 @@ const movieController = require('../controllers/movieController');
 
 const router = express.Router();
 
-router.get('/:movieId', movieController.getMovie);
-router.post('/:movieId/like', movieController.like);
-router.delete('/:movieId/like', movieController.unlike);
-router.get('/:movieId/rate', movieController.getRating);
-router.post('/:movieId/rate', movieController.rate);
-router.put('/:movieId/rate', movieController.editRate);
-
 router.get('/', movieController.searchMovie);
+router.get('/genre', movieController.getMovieByGenre);
+router.get('/id/:movieId', movieController.getMovie);
+router.post('/id/:movieId/like', movieController.like);
+router.delete('/id/:movieId/like', movieController.unlike);
+router.get('/id/:movieId/rate', movieController.getRating);
+router.post('/id/:movieId/rate', movieController.rate);
+router.put('/id/:movieId/rate', movieController.editRate);
 
 module.exports = router;
