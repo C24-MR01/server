963b6a875a45afdd15c83803ce6949a510c811b7
anonruff <jamaludinahmad013@gmail.com>
2024-06-13 02:59:40 +0700

    [fix] change syntax

--- src/utils/ageCalculation.js
+++ src/utils/ageCalculation.js
@@ -1,7 +1,7 @@
 const calculateAge = (date, today) => {
     const age = today.getFullYear() - date.getFullYear();
     const monthDiff = today.getMonth() - date.getMonth();
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday.getDate())) {
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < date.getDate())) {
         return age - 1;
     }
     return age;
